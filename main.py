3473b29f0c21c3ab474c434be79a575a48f56d7e
Dmitriy4512 <121491254+Dmitriy4512@users.noreply.github.com>
2023-01-02 20:51:39 +0200

    4 lesoon ,

---
+++ main.py
@@ -0,0 +1,17 @@
+def numbers(a ,b)->int:
+    return(a + b)
+
+print(numbers(3,44))
+
+
+
+
+# def two_numbers(a:int , b:float ) -> int:
+#     """add two numbers
+#     a - int
+#     b - int
+#     """
+#     return (a + b)
+#  # если функция не нужна, pass
+#
+# print(two_numbers(a=4 ,b=7.0 ))
